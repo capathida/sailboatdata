22a422fd817cd6ce51d6d520fb0d4bfba4f0542e
Patrik Johansson <montauk74@gmail.com>
2019-03-11 10:40:32 +0100

    draft 2

---
+++ sailingdata.py
@@ -0,0 +1,54 @@
+filename = "data/sailboat.data"
+
+def wind_data(line):
+    total_speed = 0
+    for speed in line:
+        total_speed += speed 
+    print(line, total_speed)
+
+def wind_velocity(line):
+    print("Velocity")
+    print(line)
+
+def run(line):
+    print("run")
+    print(line)
+
+def gybe(line):
+    print("gybe")
+    print(line)
+
+def beat(line):
+    print("Beat Angle")
+    print(line)
+
+def not_found(splitted_line):
+    print("Not Found", splitted_line[0])
+
+def switch_demo(splitted_line) :
+    switcher = {
+        '52°': wind_data,
+        '60°': wind_data,
+        '75°': wind_data,
+        '90°': wind_data,
+        '110°': wind_data,
+        '120°': wind_data,
+        '135°': wind_data,
+        '150°': wind_data,
+        'Run': run,
+        'Gybe': gybe,
+        'Beat': beat,
+        'Wind': wind_velocity
+    }
+    # Get the function from switcher dictionary
+    func1 = switcher.get(splitted_line[0], not_found)
+    # Execute the function
+    if func1:
+        func1(splitted_line)
+
+with open(filename) as f:
+    lines = f.read().splitlines()
+
+for line in lines:
+    splitted_line = line.split()
+    switch_demo(splitted_line)
